4d8918fac32a99665d7d9bb09cdb0c64e80b40d6
jimmy.callin <jimmy.callin@gmail.com>
2020-04-06 20:52:51 +0200

    updated push to use docker hub

--- elasticsearch-24-icu/push.sh
+++ elasticsearch-24-icu/push.sh
@@ -3,6 +3,11 @@
 # First, make sure you're logged in and your token has package:write privileges
 # echo $GITHUB_TOKEN | docker login docker.pkg.github.com -u $GITHUB_USER --password-stdin
 
-docker build -t es24 .
-docker tag es24 docker.pkg.github.com/bonniernews/packages/elasticsearch-24-icu:latest
-docker push docker.pkg.github.com/bonniernews/packages/elasticsearch-24-icu:latest
+# We REALLY want to push these to the Github Packages repository, but for some reason GitHub 
+# currently doesn't support using services from their own registry. So we use Docker Hub until 
+# they fix that
+# docker build -t docker.pkg.github.com/bonniernews/packages/elasticsearch-24-icu:latest .
+# docker push docker.pkg.github.com/bonniernews/packages/elasticsearch-24-icu:latest
+
+docker build -t bonniernews/elasticsearch-es24-icu .
+docker push bonniernews/elasticsearch-es24-icu:latest
